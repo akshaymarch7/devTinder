d911a8f9af02b28945de138656fcf39054766dd7
Akshay Saini <akshaymarch7@gmail.com>
2024-09-04 17:54:00 +0530

    playing with the routes

--- homework.md
+++ homework.md
@@ -18,3 +18,7 @@
  - Order of the routes matter a lot
  - Install Postman app and make a workspace/collectio > test API call
  - Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
+ - Explore routing and use of ?, + , (), * in the routes
+ - Use of regex in routes /a/ ,  /.*fly$/
+ - Reading the query params in the routes
+ - Reading the dynamic routes
--- src/app.js
+++ src/app.js
@@ -2,24 +2,9 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-
-app.post("/user", async (req, res) => {
-  console.log(req.body);
-  // saving data to DB
-  res.send("Data successfully saved to the database!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
 });
 
 app.listen(7777, () => {
