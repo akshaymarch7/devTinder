74dc4f32c96cb44d93e94bd91d150a4608130e96
Akshay Saini <akshaymarch7@gmail.com>
2024-09-06 00:59:45 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
+//app.use("/route", rH, [rH2, rH3], rH4, rh5);
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
