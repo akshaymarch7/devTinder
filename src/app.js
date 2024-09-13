205c577676272894706566df20efa3d41069c3b0
Akshay Saini <akshaymarch7@gmail.com>
2024-09-13 14:30:13 +0530

    created login API

--- src/app.js
+++ src/app.js
@@ -33,6 +33,26 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
 // Get user by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
