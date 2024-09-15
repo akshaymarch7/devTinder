8191291a1e9b4110f35d24142769041da5a8476f
Akshay Saini <akshaymarch7@gmail.com>
2024-09-15 23:55:56 +0530

    feed API + pagination

--- homework.md
+++ homework.md
@@ -117,3 +117,28 @@
  - Read about ref and populate https://mongoosejs.com/docs/populate.html
  - Create GET /user/requests/received with all the checks
  - Create GET GET /user/connections
+
+ - Logic for GET /feed API
+ - Explore the $nin , $and, $ne and other query operatorators
+ - Pagination
+
+
+
+NOTES: 
+
+ /feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)
+
+ /feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)
+
+ /feed?page=3&limit=10 => 21-30 => .skip(20) & .limit(10)
+
+ /feed?page=4&limit=10 => 21-30 => .skip(20) & .limit(10)
+
+skip = (page-1)*limit;
+
+ 
+
+
+
+
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -51,6 +52,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({ data });
   } catch (err) {
     res.status(400).send({ message: err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId  toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 module.exports = userRouter;
